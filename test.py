325c7c735790b1abaa71e3817aa598ad75069fde
vas-ide <vas.ide2022@gmail.com>
2022-07-20 13:53:37 +0300

    Harrison continue
    chapter 13 in progress

---
+++ test.py
@@ -0,0 +1,17 @@
+def get_middle(s):
+    #your code here
+    if len(s) <= 2:
+        print(s)
+        return s
+    else:
+        counter = 0
+        for i in s:
+            result = ""
+            if len(s) % 2 == 0:
+
+
+
+
+get_middle("te")
+get_middle("test")
+
